690fe9ff370d2d41c20af2aee685b32e66a73c00
Thief8925 <asallweb@gmail.com>
2022-03-22 15:38:07 +0200

    v1.2

--- js/main.js
+++ js/main.js
@@ -4,12 +4,20 @@ $(document).ready(function () {
 	// 	slidesToShow: 8,
 	// 	slidesToScroll: 1
 	// });
-	var swiper = new Swiper(".stories", {
+	var swiper = new Swiper(".stories-main .swiper", {
 		slidesPerView: 8,
-		spaceBetween: 18,
+		spaceBetween: 17,
 		navigation: {
-			nextEl: ".swiper-button-next",
-			prevEl: ".swiper-button-prev",
+			nextEl: ".stories-main .swiper-button-next",
+			prevEl: ".stories-main .swiper-button-prev",
+		},
+	});
+	var swiper = new Swiper(".stories-list .swiper", {
+		slidesPerView: 4,
+		spaceBetween: 17,
+		navigation: {
+			nextEl: ".stories-list .swiper-button-next",
+			prevEl: ".stories-list .swiper-button-prev",
 		},
 	});
 });
