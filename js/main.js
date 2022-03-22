c64491be24f8fae2c0acd1515a91b58abd4453fe
Thief8925 <asallweb@gmail.com>
2022-03-22 12:40:56 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+	// $('.stories').slick({
+	// 	infinite: true,
+	// 	slidesToShow: 8,
+	// 	slidesToScroll: 1
+	// });
+	var swiper = new Swiper(".stories", {
+		slidesPerView: 8,
+		spaceBetween: 18,
+	});
+});
+$(window).resize(function () {
+
+});
