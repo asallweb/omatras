fa45b87878818496e1adcec7bd4bdce90d4515ce
Thief8925 <asallweb@gmail.com>
2022-03-22 13:05:34 +0200

    v1.1

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ $(document).ready(function () {
 	var swiper = new Swiper(".stories", {
 		slidesPerView: 8,
 		spaceBetween: 18,
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
 	});
 });
 $(window).resize(function () {
